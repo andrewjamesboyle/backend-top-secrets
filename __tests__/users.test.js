553c31ef0c06146966d162e91e2f10108c763d53
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-16 15:42:41 -0800

    create user test written

---
+++ __tests__/users.test.js
@@ -0,0 +1,33 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  it('creates a new user', async () => {
+    const res = await (await request(app).post('/api/v1/users')).setEncoding(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
