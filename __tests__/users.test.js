84e41bf78a702e4d131b1ede42482e1777b585c4
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-11-16 17:24:00 -0800

    sign in a user test written

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Test',
@@ -10,7 +11,17 @@ const mockUser = {
   password: '12345',
 };
 
-describe('backend-express-template routes', () => {
+// Register and Login Function for Testing
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('top-secret users tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -24,6 +35,17 @@ describe('backend-express-template routes', () => {
       firstName,
       lastName,
       email,
+    });
+  });
+
+  it('signs in and returns user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
 
